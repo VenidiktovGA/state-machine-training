8d203cb127ab432904097666271044f24756b987
GennadyVenidiktov <wenter3@outlook.com>
2024-02-10 16:41:49 +0500

    init

---
+++ src/main/java/ru/venidiktov/state/machine/training/StateMachineTrainingApplication.java
@@ -0,0 +1,13 @@
+package ru.venidiktov.state.machine.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateMachineTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateMachineTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/venidiktov/state/machine/training/StateMachineTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.venidiktov.state.machine.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StateMachineTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
